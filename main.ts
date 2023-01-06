801ee006092a068016b19ac997b1dca36fdb2d66
spike <ololmkbc.dev@gmail.com>
2023-01-06 20:43:05 +0200

    feat: add objects for tests

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { a1, b1, a2, b2, a3, b3, a4, b4, a5, b5 } from "./objects";
+import {checkObjects, deepClone, deepIteration} from "./deepclone";
+
+function test() {
+    console.log('FIRST')
+    console.log(checkObjects(a1, b1))  // b1.size.width = 100
+    console.log('SECOND')
+    console.log(checkObjects(a2, b2)) // skipped b2.size.width
+    console.log('THIRD')
+    console.log(checkObjects(a3, b3)) // skipped a3.size.block.width
+    console.log('FOURTH')
+    console.log(checkObjects(a4, b4)) // skipped a4.size.height and skipped b4.size.block.height
+    console.log('FIFTH')
+    console.log(checkObjects(a5, b5)) // equal
+}
+
+test()
---
+++ objects.ts
@@ -0,0 +1,129 @@
+export const a1 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 10,
+        height: 10,
+        block: {
+            isValid: true,
+            width: 30,
+            height: 30
+        }
+    }
+}
+export const b1 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 100,
+        height: 10,
+        block: {
+            isValid: true,
+            width: 30,
+            height: 30
+        }
+    }
+}
+
+
+export const a2 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 10,
+        height: 10,
+        block: {
+            isValid: true,
+            width: 30,
+            height: 30
+        }
+    }
+}
+export const b2 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        height: 10,
+        block: {
+            isValid: true,
+            width: 30,
+            height: 30
+        }
+    }
+}
+
+export const a3 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 10,
+        height: 10,
+        block: {
+            isValid: true,
+            height: 30
+        }
+    }
+}
+export const b3 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 10,
+        height: 10,
+        block: {
+            isValid: true,
+            width: 30,
+            height: 30
+        }
+    }
+}
+
+export const a4 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 10,
+        height: 10,
+        block: {
+            isValid: true,
+            width: 30,
+        }
+    }
+}
+export const b4 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 10,
+        block: {
+            isValid: true,
+            width: 30,
+            height: 30
+        }
+    }
+}
+
+export const a5 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 10,
+        height: 10,
+        block: {
+            isValid: true,
+            width: 30,
+        }
+    }
+}
+export const b5 = {
+    color: 'white',
+    isValid: true,
+    size: {
+        width: 10,
+        height: 10,
+        block: {
+            isValid: true,
+            width: 30,
+        }
+    }
+}
